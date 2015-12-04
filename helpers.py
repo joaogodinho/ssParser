9f0a676c7d31aef511381e5e6054ff13a2602501
joaogodinho <joaocfgodinho@gmail.com>
2015-12-04 23:05:18 +0000

    Step 1 and 2 done.

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Pattern:
     @staticmethod
     def parseFile(file):
@@ -21,6 +24,8 @@ class Pattern:
         self.entry_points = entry_points
         self.val_func = val_func
         self.sinks = sinks
+        self.calledSinks = []
+        self.calledEntries = []
 
     # To string method
     def __str__(self):
@@ -43,12 +48,38 @@ class Pattern:
 
 class Trace:
     def __init__(self, file):
-        self.lines = []
+        lines = []
         with open(file, 'r') as f:
-            self.lines = [l for l in f]
-        assert "TRACE START" in self.lines[0] and "TRACE END" in self.lines[-1], \
+            lines = [l.strip() for l in f]
+            lines = filter(None, lines)
+        assert "TRACE START" in lines[0] and "TRACE END" in lines[-1], \
             "Invalid trace file"
-        self.lines = self.lines[1:-1]
+        lines = lines[1:-1]
+        self.calls = []
+        for l in lines:
+            call = TraceCall.parseCall(l)
+            if call:
+                self.calls.append(call)
+
+    def checkSinks(self, pattern):
+        """
+        Takes a pattern and checks if any sinks exist in the
+        trace file. If they do, return a list the cases.
+        """
+        matches = []
+        for call in self.calls:
+            for s in pattern.sinks:
+                if s in call.func:
+                    matches.append(call)
+        return matches
+
+    def checkSanitization(self, pattern):
+        matches = []
+        for call in self.calls:
+            for s in pattern.val_func:
+                if s in call.func:
+                    matches.append(call)
+        return matches
 
     # To string method
     def __str__(self):
@@ -63,6 +94,43 @@ class Trace:
         for l in self.lines:
             format += l
         return format
+
+
+class TraceCall:
+    # Frist group: function/object call
+    # Second group: file location
+    # Thrid group: line number
+    REG_EXP = r'->\s+(\S+)\(.*\)\s+(\S+):(\d+)'
+    # Make it a const, no need to compile same pattern multiple times
+    REG_OBJ = re.compile(REG_EXP)
+
+    @staticmethod
+    def parseCall(line):
+        matchObj = TraceCall.REG_OBJ.search(line)
+        if matchObj is not None:
+            groups = matchObj.groups()
+            assert groups is not None and len(groups) == 3, "Invalid line from trace"
+            func = groups[0]
+            file = groups[1]
+            line = groups[2]
+            return TraceCall(func, file, line)
+        return None
+
+    def __init__(self, func, file, line):
+        self.func = func
+        self.file = file
+        self.line = line
+
+    # To string method
+    def __str__(self):
+        format = "Function: %s from file %s in line: %s" % (self.func, self.file, self.line)
+        return format
+
+    # To string method
+    def __unicode__(self):
+        format = "Function: %s from file %s in line: %s" % (self.func, self.file, self.line)
+        return format
+
 
 # TODO check this
 # class Warning:
--- main.py
+++ main.py
@@ -1,11 +1,116 @@
 #!/usr/bin/python
 import argparse
+import re
 from helpers import Pattern, Trace
 
 
 def main(args):
     patterns = getPatterns(args.file_pattern[0])
     trace = loadTrace(args.file_trace[0])
+    if runStep1a(patterns, trace):
+        print "Step 1a FAILED"
+        if runStep1b(patterns):
+            print "Step 1b FAILED"
+            runStep2(patterns, trace)
+            if runStep3(patterns):
+                print "Step 3 FAILED"
+            else:
+                print "Step 3 PASSED"
+        else:
+            print "Step 1b PASSED"
+    else:
+        print "Step 1a PASSED"
+    print "Analysis complete"
+
+
+def runStep1a(patterns, trace):
+    print ""
+    print "Running Step 1a, checking for sinks in trace file..."
+    print ""
+    vuln = False
+    for i, p in enumerate(patterns):
+        print "Checking sinks from pattern #%s (%s)..." % (str(i+1), p.vuln)
+        hits = trace.checkSinks(p)
+        if hits:
+            vuln = True
+            print "Got %d hits from this pattern:" % len(hits)
+            for call in hits:
+                print call
+            p.calledSinks = hits
+        else:
+            print "No hits from this pattern."
+        print ""
+    return vuln
+
+
+def runStep1b(patterns):
+    print ""
+    print "Running Step 1b, checking for entry points in sources..."
+    print ""
+    vuln = False
+    for i, p in enumerate(patterns):
+        matches = []
+        if p.calledSinks:
+            print "Checking entries from pattern #%s (%s)..." % (str(i+1), p.vuln)
+            for point in p.entry_points:
+                reg_obj = re.compile(point.replace('$', '\$'))
+                hits = []
+                files = list(set([c.file for c in p.calledSinks]))
+                for file in files:
+                    with open(file, 'r') as f:
+                        lnumb = 1
+                        for line in f:
+                            matchObj = reg_obj.search(line)
+                            if matchObj:
+                                hits.append([lnumb, file])
+                            lnumb += 1
+                if hits:
+                    vuln = True
+                    matches.append([point, hits])
+                    print "Got %d hits from %s:" % (len(hits), point)
+                    for hit in hits:
+                        print "File %s, line %d" % (hit[1], hit[0])
+                else:
+                    print "No hits from %s" % point
+                print ""
+        p.calledEntries = matches
+    return vuln
+
+
+def runStep2(patterns, trace):
+    print ""
+    print "Running Step 2, comparing sinks and sanization..."
+    print ""
+    vuln = False
+    for i, p in enumerate(patterns):
+        if p.calledSinks:
+            print "Checking sanitization from pattern #%s (%s)..." % (str(i+1), p.vuln)
+            hits = trace.checkSanitization(p)
+            if hits:
+                vuln = True
+                print "Got %d hits from this pattern:" % len(hits)
+                for call in hits:
+                    print call
+                p.calledSinks = hits
+            else:
+                print "No hits from this pattern."
+            nentry_points = 0
+            for e in p.calledEntries:
+                nentry_points += len(e[1])
+            print ""
+            print "Stats for pattern #%s (%s):" % (str(i+1), p.vuln)
+            print "Entry points = %d\tSanitization functions = %d\tSinks = %d" \
+                % (nentry_points, len(hits), len(p.calledSinks))
+    return vuln
+
+
+def runStep3(patterns):
+    print ""
+    print "Running Step 3, flow between sinks and entries..."
+    print ""
+    print "NOT IMPLEMENTED"
+    print ""
+    return True
 
 
 def getPatterns(file):
